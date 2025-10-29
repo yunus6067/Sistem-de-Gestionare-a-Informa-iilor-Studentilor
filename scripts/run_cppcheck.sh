5aade98a85d19487cde78d627253981b0187fc14
yunus6067 <yunuskarataev1919@gmail.com>
2025-10-29 17:12:02 +0200

    Initial commit

---
+++ scripts/run_cppcheck.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+cppcheck --enable=all \
+    --inline-suppr \
+    --project="${BUILD_DIR:-build}"/compile_commands.json \
+    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
+    -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
+    --suppress=missingIncludeSystem \
+    --suppress=unmatchedSuppression \
+    --suppress=useStlAlgorithm \
+    --check-level=exhaustive \
+    --error-exitcode=1
